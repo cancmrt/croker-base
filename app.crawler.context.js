e1afe1c4f007fc21581d0532863653e0ca9bb596
cancmrt <cancm91@gmail.com>
2022-12-08 17:28:23 +0300

    son değişikliler

--- app.crawler.context.js
+++ app.crawler.context.js
@@ -19,6 +19,13 @@ export class CrawlerResult {
   IsError = true
   Error = undefined
 };
+export class CrawlerResultJSON {
+  URL = ''
+  HttpMethod = HttpMethodType.GET
+  JSON = ''
+  IsError = true
+  Error = undefined
+};
 
 export class CrokerCrawler {
   RemoveTagAndWhiteSpaces (html) {
@@ -30,7 +37,8 @@ export class CrokerCrawler {
      */
   async Load (Req) {
     const result = new CrawlerResult()
-
+    result.URL = Req.URL
+    result.HttpMethod = Req.HttpMethod
     try {
       let URLResponse = ''
       if (Req.HttpMethod === HttpMethodType.GET) {
@@ -48,6 +56,48 @@ export class CrokerCrawler {
       }
       result.PageHTML = URLResponse
       result.Document = cheerio.load(result.PageHTML)
+      result.IsError = false
+    } catch (e) {
+      result.IsError = true
+      result.Error = e
+    }
+    return result
+  }
+
+  /**
+     * @param {CrawlerLoader} Req - The date
+     */
+  async LoadJSON (Req) {
+    const result = new CrawlerResultJSON()
+
+    try {
+      let URLResponse = ''
+      result.URL = Req.URL
+      result.HttpMethod = Req.HttpMethod
+      if (Req.HttpMethod === HttpMethodType.GET) {
+        const { data, status } = await axios.get(Req.URL, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          timeout: 60000
+        })
+        if (status !== 200) {
+          throw Error('İstenilen adrese ulaşılamadı.')
+        }
+        URLResponse = data
+      } else if (Req.HttpMethod === HttpMethodType.POST) {
+        const { data, status } = await axios.post(Req.URL, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          timeout: 60000
+        })
+        if (status !== 200) {
+          throw Error('İstenilen adrese ulaşılamadı.')
+        }
+        URLResponse = data
+      }
+      result.JSON = URLResponse
       result.IsError = false
     } catch (e) {
       result.IsError = true
